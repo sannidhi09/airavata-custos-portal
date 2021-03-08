3b7d373a504d4ea7c0f4ac9f8257a257431de31b
Dinuka De Silva <l.dinukadesilva@gmail.com>
2021-03-08 02:13:13 -0500

    Adding SCSS and bootstrap theming

--- custos-demo-gateway/src/main.js
+++ custos-demo-gateway/src/main.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router.js";
 import store from './store/index.js'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import Vuelidate from 'vuelidate'
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './styles.scss';
 
 Vue.use(BootstrapVue)
 Vue.use(Vuelidate)
@@ -16,9 +15,9 @@ Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
 
 
