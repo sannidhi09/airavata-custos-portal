517e9a693d6ff080e3c9ab6bd9e3dc672baa7e5e
Dinuka De Silva <l.dinukadesilva@gmail.com>
2021-06-10 02:36:44 -0400

    input validations on the tenant profile edit page

--- custos-demo-gateway/src/components/admin-portal/TenantProfile.vue
+++ custos-demo-gateway/src/components/admin-portal/TenantProfile.vue
@@ -1,30 +1,170 @@
 <template>
-  <TenantHome title="Profile" :breadcrumb-links="breadcrumbLinks">
-    <div>
-      <div style="max-width: 600px;">
-        <div class="pt-3" style="display: flex; flex-direction: row;">
+  <TenantHome title="Profile" :breadcrumb-links="breadcrumbLinks" :errors="errors">
+    <b-overlay :show="processing">
+      <div class="p-2 text-center">
+        <div class="text-left" style="max-width: 600px; display: inline-block;">
+          <div class="pt-3" style="display: flex; flex-direction: row;">
 
-          <div style="flex: 1;" class="mr-2">
-            <label class="form-label" for="username">Username</label>
+            <div style="flex: 1;" class="mr-2">
+              <label class="form-label" for="username">Username</label>
+              <b-form-input
+                  v-model="username"
+                  :state="inputState.username"
+                  id="username"
+                  trim
+                  size="sm"
+                  readonly>
+              </b-form-input>
+              <b-form-invalid-feedback>
+                Enter at least 3 letters
+              </b-form-invalid-feedback>
+            </div>
+
+            <div style="flex: 1;" class="mr-2">
+              <label class="form-label" for="first-name">First Name</label>
+              <b-form-input
+                  v-model="firstName"
+                  :state="inputState.firstName"
+                  id="first-name"
+                  trim
+                  size="sm">
+              </b-form-input>
+              <b-form-invalid-feedback>
+              </b-form-invalid-feedback>
+            </div>
+
+            <div style="flex: 1;" class="ml-2">
+              <label class="form-label" for="last-name">Last Name</label>
+              <b-form-input
+                  v-model="lastName"
+                  :state="inputState.lastName"
+                  id="last-name"
+                  trim
+                  size="sm">
+              </b-form-input>
+              <b-form-invalid-feedback>
+              </b-form-invalid-feedback>
+            </div>
+          </div>
+
+          <div class="pt-3">
+            <label class="form-label" for="email">Email</label>
             <b-form-input
-                v-model="username"
-                :state="inputState.username"
-                id="username"
+                v-model="email"
+                :state="inputState.email"
+                type="email"
+                id="email"
                 trim
+                size="sm">
+            </b-form-input>
+            <b-form-invalid-feedback>
+            </b-form-invalid-feedback>
+          </div>
+
+          <div class="pt-3">
+            <label class="form-label" for="tenantName">Tenant Name</label>
+            <b-form-input
+                v-model="tenantName"
+                :state="inputState.tenantName"
+                id="tenantName"
+                trim
+                size="sm"
+                placeholder="Name of your OAuth 2.0 client"
+                aria-describedby="tenantName-help-block">
+            </b-form-input>
+            <b-form-text id="tenantName-help-block">
+              The Client Name is displayed to end-users on the Identity Provider selection page.
+            </b-form-text>
+            <b-form-invalid-feedback>
+            </b-form-invalid-feedback>
+          </div>
+
+          <div class="pt-3">
+            <label class="form-label" for="clientId">Client ID</label>
+            <b-form-input
+                v-model="clientId"
+                id="clientId"
                 size="sm"
                 readonly>
             </b-form-input>
+          </div>
+
+          <div class="pt-3">
+            <label class="form-label" for="clientSecret">Secret</label>
+            <b-form-input
+                v-model="clientSecret"
+                id="clientSecret"
+                size="sm"
+                readonly>
+            </b-form-input>
+          </div>
+
+          <div class="pt-3">
+            <label class="form-label">Redirect URI</label>
+            <div class="pb-2" v-for="(redirectUri, redirectUriIndex) in redirectUris" :key=redirectUriIndex>
+              <label class="form-label" :for="`redirectUri-${redirectUriIndex}`"
+                     style="visibility: hidden;line-height: 0px;margin: 0px;position: absolute;">
+                Redirect URI {{ redirectUriIndex }}</label>
+              <b-input-group>
+                <b-form-input
+                    v-model="redirectUris[redirectUriIndex]"
+                    :state="inputState.redirectUris[redirectUriIndex]"
+                    :id="`redirectUri-${redirectUriIndex}`"
+                    trim
+                    type="url"
+                    size="sm">
+                </b-form-input>
+                <b-input-group-append>
+                  <b-button size="sm" v-if="redirectUris.length > 1"
+                            v-on:click="redirectUris = redirectUris.filter((r,i)=> i !== redirectUriIndex)">
+                    Remove
+                  </b-button>
+                </b-input-group-append>
+              </b-input-group>
+              <b-form-invalid-feedback>
+              </b-form-invalid-feedback>
+            </div>
+            <b-button size="sm" variant="link" v-on:click="redirectUris.push('')">Add another URI</b-button>
+          </div>
+
+          <div class="pt-3">
+            <label class="form-label" for="scope">Scope</label>
+            <b-form-checkbox-group
+                v-model="scope"
+                :options="['openid', 'email', 'profile', 'org.cilogon.userinfo']"
+                :state="inputState.scope"
+                id="scope"
+                trim
+                size="sm"
+                aria-describedby="scope-help-block">
+            </b-form-checkbox-group>
+            <b-form-text id="scope-help-block">
+              <b-link href="https://www.cilogon.org/oidc#h.p_PEQXL8QUjsQm">Information on scopes</b-link>
+            </b-form-text>
             <b-form-invalid-feedback>
-              Enter at least 3 letters
             </b-form-invalid-feedback>
           </div>
 
-          <div style="flex: 1;" class="mr-2">
-            <label class="form-label" for="first-name">First Name</label>
+          <div class="pt-3">
+            <label class="form-label" for="domain">Domain</label>
             <b-form-input
-                v-model="firstName"
-                :state="inputState.firstName"
-                id="first-name"
+                v-model="domain"
+                :state="inputState.domain"
+                id="domain"
+                trim
+                size="sm"
+                placeholder="idp.htrc.indiana.edu">
+            </b-form-input>
+            <b-form-invalid-feedback>
+            </b-form-invalid-feedback>
+          </div>
+
+          <div class="pt-3">
+            <label class="form-label" for="clientUri">Client URI</label>
+            <b-form-input
+                v-model="clientUri"
+                :state="inputState.clientUri"
+                id="clientUri"
                 trim
                 size="sm">
             </b-form-input>
@@ -32,192 +172,54 @@
             </b-form-invalid-feedback>
           </div>
 
-          <div style="flex: 1;" class="ml-2">
-            <label class="form-label" for="last-name">Last Name</label>
+          <div class="pt-3">
+            <label class="form-label" for="logoUri">Logo URI</label>
             <b-form-input
-                v-model="lastName"
-                :state="inputState.lastName"
-                id="last-name"
+                v-model="logoUri"
+                :state="inputState.logoUri"
+                id="logoUri"
                 trim
                 size="sm">
             </b-form-input>
             <b-form-invalid-feedback>
             </b-form-invalid-feedback>
           </div>
-        </div>
 
-        <div class="pt-3">
-          <label class="form-label" for="email">Email</label>
-          <b-form-input
-              v-model="email"
-              :state="inputState.email"
-              type="email"
-              id="email"
-              trim
-              size="sm">
-          </b-form-input>
-          <b-form-invalid-feedback>
-          </b-form-invalid-feedback>
-        </div>
-
-        <div class="pt-3">
-          <label class="form-label" for="tenantName">Tenant Name</label>
-          <b-form-input
-              v-model="tenantName"
-              :state="inputState.tenantName"
-              id="tenantName"
-              trim
-              size="sm"
-              placeholder="Name of your OAuth 2.0 client"
-              aria-describedby="tenantName-help-block">
-          </b-form-input>
-          <b-form-text id="tenantName-help-block">
-            The Client Name is displayed to end-users on the Identity Provider selection page.
-          </b-form-text>
-          <b-form-invalid-feedback>
-          </b-form-invalid-feedback>
-        </div>
-
-        <div class="pt-3">
-          <label class="form-label" for="clientId">Client ID</label>
-          <b-form-input
-              v-model="clientId"
-              id="clientId"
-              size="sm"
-              readonly>
-          </b-form-input>
-        </div>
-
-        <div class="pt-3">
-          <label class="form-label" for="clientSecret">Secret</label>
-          <b-form-input
-              v-model="clientSecret"
-              id="clientSecret"
-              size="sm"
-              readonly>
-          </b-form-input>
-        </div>
-
-        <div class="pt-3">
-          <label class="form-label">Redirect URI</label>
-          <div class="pb-2" v-for="(redirectUri, redirectUriIndex) in redirectUris" :key=redirectUriIndex>
-            <label class="form-label" :for="`redirectUri-${redirectUriIndex}`"
-                   style="visibility: hidden;line-height: 0px;margin: 0px;position: absolute;">
-              Redirect URI {{ redirectUriIndex }}</label>
-            <b-input-group>
-              <b-form-input
-                  v-model="redirectUris[redirectUriIndex]"
-                  :state="inputState.redirectUris[redirectUriIndex]"
-                  :id="`redirectUri-${redirectUriIndex}`"
-                  trim
-                  type="url"
-                  size="sm">
-              </b-form-input>
-              <b-input-group-append>
-                <b-button size="sm" v-if="redirectUris.length > 1"
-                          v-on:click="redirectUris = redirectUris.filter((r,i)=> i !== redirectUriIndex)">
-                  Remove
-                </b-button>
-              </b-input-group-append>
-            </b-input-group>
+          <div class="pt-3">
+            <label class="form-label" for="comment">Comment</label>
+            <b-form-input
+                v-model="comment"
+                :state="inputState.comment"
+                id="comment"
+                trim
+                size="sm"
+                placeholder="Provide description of the new tenant and any other comments here (at least 15 characters long)">
+            </b-form-input>
             <b-form-invalid-feedback>
             </b-form-invalid-feedback>
           </div>
-          <b-button size="sm" variant="link" v-on:click="redirectUris.push('')">Add another URI</b-button>
-        </div>
 
-        <div class="pt-3">
-          <label class="form-label" for="scope">Scope</label>
-          <b-form-checkbox-group
-              v-model="scope"
-              :options="['openid', 'email', 'profile', 'org.cilogon.userinfo']"
-              :state="inputState.scope"
-              id="scope"
-              trim
-              size="sm"
-              aria-describedby="scope-help-block">
-          </b-form-checkbox-group>
-          <b-form-text id="scope-help-block">
-            <b-link href="https://www.cilogon.org/oidc#h.p_PEQXL8QUjsQm">Information on scopes</b-link>
-          </b-form-text>
-          <b-form-invalid-feedback>
-          </b-form-invalid-feedback>
-        </div>
+          <div class="pt-3">
+            <label class="form-label" for="applicationType">Application Type</label>
+            <b-form-radio-group
+                :options="['web']"
+                v-model="applicationType"
+                :state="inputState.applicationType"
+                id="applicationType"
+                trim
+                size="sm">
+            </b-form-radio-group>
+            <b-form-invalid-feedback>
+            </b-form-invalid-feedback>
+          </div>
 
-        <div class="pt-3">
-          <label class="form-label" for="domain">Domain</label>
-          <b-form-input
-              v-model="domain"
-              :state="inputState.domain"
-              id="domain"
-              trim
-              size="sm"
-              placeholder="idp.htrc.indiana.edu">
-          </b-form-input>
-          <b-form-invalid-feedback>
-          </b-form-invalid-feedback>
         </div>
-
-        <div class="pt-3">
-          <label class="form-label" for="clientUri">Client URI</label>
-          <b-form-input
-              v-model="clientUri"
-              :state="inputState.clientUri"
-              id="clientUri"
-              trim
-              size="sm">
-          </b-form-input>
-          <b-form-invalid-feedback>
-          </b-form-invalid-feedback>
+        <div>
+          <b-button variant="primary" size="sm" v-on:click="onClickSave">Save Changes</b-button>
+          <b-button variant="secondary" size="sm" class="ml-2">Cancel</b-button>
         </div>
-
-        <div class="pt-3">
-          <label class="form-label" for="logoUri">Logo URI</label>
-          <b-form-input
-              v-model="logoUri"
-              :state="inputState.logoUri"
-              id="logoUri"
-              trim
-              size="sm">
-          </b-form-input>
-          <b-form-invalid-feedback>
-          </b-form-invalid-feedback>
-        </div>
-
-        <div class="pt-3">
-          <label class="form-label" for="comment">Comment</label>
-          <b-form-input
-              v-model="comment"
-              :state="inputState.comment"
-              id="comment"
-              trim
-              size="sm"
-              placeholder="Provide description of the new tenant and any other comments here (at least 15 characters long)">
-          </b-form-input>
-          <b-form-invalid-feedback>
-          </b-form-invalid-feedback>
-        </div>
-
-        <div class="pt-3">
-          <label class="form-label" for="applicationType">Application Type</label>
-          <b-form-radio-group
-              :options="['web']"
-              v-model="applicationType"
-              :state="inputState.applicationType"
-              id="applicationType"
-              trim
-              size="sm">
-          </b-form-radio-group>
-          <b-form-invalid-feedback>
-          </b-form-invalid-feedback>
-        </div>
-
       </div>
-      <div>
-        <b-button variant="primary" size="sm" v-on:click="onClickSave">Save Changes</b-button>
-        <b-button variant="secondary" size="sm" class="ml-2">Cancel</b-button>
-      </div>
-    </div>
+    </b-overlay>
   </TenantHome>
 </template>
 
@@ -232,6 +234,9 @@ export default {
   components: {TenantHome},
   data() {
     return {
+      processing: false,
+      errors: [],
+
       username: null,
       firstName: null,
       lastName: null,
@@ -250,7 +255,7 @@ export default {
 
       clientSecret: null,
 
-      inputFieldsList: ["firstName", "lastName", "email", "tenantName", "redirectUris", "scope", "domain", "clientUri",
+      inputFieldsList: ["firstName", "lastName", "email", "tenantName", "redirectUris", "domain", "clientUri", "logoUri",
         "comment", "applicationType"]
     }
   },
@@ -276,30 +281,33 @@ export default {
         email: this.email === null ? null : this.isValid.email,
 
         tenantName: this.tenantName === null ? null : this.isValid.tenantName,
-        redirectUris: [null],
+        redirectUris: this.redirectUris.map((redirectUri, redirectUriIndex) => {
+          return this.redirectUris[redirectUriIndex] === null ? null : this.isValid.redirectUris[redirectUriIndex];
+        }),
         scope: null,
-        domain: null,
-        clientUri: null,
-        logoUri: null,
-        comment: null,
-        applicationType: null
+        domain: this.domain === null ? null : this.isValid.domain,
+        clientUri: this.clientUri === null ? null : this.isValid.clientUri,
+        logoUri: this.logoUri === null ? null : this.isValid.logoUri,
+        comment: this.comment === null ? null : this.isValid.comment,
+        applicationType: this.applicationType === null ? null : this.isValid.applicationType
       }
     },
     isValid() {
       return {
-        username: !!this.username && this.username.length > 0,
+        username: !!this.username && this.username.length >= 3,
         firstName: !!this.firstName && this.firstName.length > 0,
         lastName: !!this.lastName && this.lastName.length > 0,
         email: !!this.email && this.email.length > 0,
 
         tenantName: !!this.tenantName && this.tenantName.length > 0,
-        redirectUris: [false],
-        scope: false,
-        domain: false,
-        clientUri: false,
-        logoUri: false,
-        comment: false,
-        applicationType: false
+        redirectUris: this.redirectUris.map(redirectUri =>
+            /^((http|https):\/\/)(www.)?[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9].*/.test(redirectUri)),
+        scope: true,
+        domain: /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/.test(this.domain),
+        clientUri: /^((http|https):\/\/)(www.)?[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9].*/.test(this.clientUri),
+        logoUri: /^((http|https):\/\/)(www.)?[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9].*/.test(this.logoUri),
+        comment: true,
+        applicationType: true
       }
     },
     isFormValid() {
@@ -344,26 +352,37 @@ export default {
     async onClickSave() {
       this.makeFormVisited();
       if (this.isFormValid) {
-        await this.$store.dispatch("tenant/updateTenant", {
-          clientId: this.clientId,
-          tenantId: this.tenantId,
+        this.processing = true;
 
-          username: this.username,
-          firstName: this.firstName,
-          lastName: this.lastName,
-          email: this.email,
+        try {
+          await this.$store.dispatch("tenant/updateTenant", {
+            clientId: this.clientId,
+            tenantId: this.tenantId,
 
-          tenantName: this.tenantName,
-          redirectUris: this.redirectUris,
-          scope: this.scope,
-          domain: this.domain,
-          clientUri: this.clientUri,
-          logoUri: this.logoUri,
-          comment: this.comment,
-          applicationType: this.applicationType,
+            username: this.username,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            email: this.email,
 
-          requesterEmail: this.requesterEmail
-        });
+            tenantName: this.tenantName,
+            redirectUris: this.redirectUris,
+            scope: this.scope,
+            domain: this.domain,
+            clientUri: this.clientUri,
+            logoUri: this.logoUri,
+            comment: this.comment,
+            applicationType: this.applicationType,
+
+            requesterEmail: this.requesterEmail
+          });
+        } catch (error) {
+          this.errors.push({
+            title: "Unknown error when updating the tenant.",
+            source: error, variant: "danger"
+          });
+        }
+
+        this.processing = false;
       }
     }
   },
