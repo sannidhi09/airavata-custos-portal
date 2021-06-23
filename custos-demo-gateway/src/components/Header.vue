ded84d828687cccc1768b1f18d0c15870a2a6aa3
Dinuka De Silva <l.dinukadesilva@gmail.com>
2021-06-23 16:52:10 -0400

    Change reference portal header title to "IoU Health"

--- custos-demo-gateway/src/components/Header.vue
+++ custos-demo-gateway/src/components/Header.vue
@@ -4,7 +4,7 @@
       <div class="custos-logo text-primary">
         <div style="font-size: 23px; font-weight: 600; font-style: italic;">
           <b-icon icon="shield-fill-plus"/>
-          IU Health
+          IoU Health
         </div>
         <!--        <img src="../assets/custos-logo_custos-logo-color-v1.png" style="width: 140px;">-->
       </div>
